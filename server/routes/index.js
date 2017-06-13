5f18ebc8db8102e7b8e479fe267551051085c8d7
Frank Matranga <thefrankmatranga@gmail.com>
2017-06-13 13:00:05 -0400

    Setup page start

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,9 +11,28 @@ router.get('/about', (req, res) => {
     res.render('index/about');
 });
 
-router.get('/setup', requireLogin, (req, res) => {
+router.get('/setup', requireLogin, (req, res, next) => {
     res.locals.pageTitle = 'Setup';
-    res.render('index/setup');
+    /* Get list of possible superiors (all non-Teachers), and locations */
+
+    req.db.User.find({ rank: { $gt: 0 } }) // Greater than 0 because 0 means Teacher
+        .exec()
+        .then(superiors => {
+            res.locals.superiors = superiors;
+
+            return req.db.Location.find();
+        })
+        .then(locations => {
+            res.locals.locations = locations;
+
+            res.render('index/setup');
+        })
+        .catch(next);
+});
+
+/* Set all new user data and set accountStatus to 1 */
+router.post('/setup', requireLogin, (req, res, next) => {
+
 });
 
 router.get('/login', (req, res) => {
