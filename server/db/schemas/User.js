cfacba38c399f0a64e7c3d2e4a1c2e6e9e862d60
Frank Matranga <thefrankmatranga@gmail.com>
2017-06-13 00:33:12 -0400

    Finished user schema

--- server/db/schemas/User.js
+++ server/db/schemas/User.js
@@ -4,12 +4,15 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     _id: { type: Number },
     email: { type: String, unique: true },
+    phoneNumber: { type: String, unique: true },
+    location: { type: mongoose.Schema.Types.ObjectId, ref: 'Location' },
     name: {
         first: { type: String, trim: true },
         last: { type: String, trim: true }
     },
     rank: { type: Number, default: 0 },
     registeredDate: { type: Date, default: Date.now },
+    superior: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     accountStatus: { type: Number, default: 0 }
 });
 
