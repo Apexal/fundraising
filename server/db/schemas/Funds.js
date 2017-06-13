bbd025b81e9f7dcffc566e9e7029ffd25f249b90
Frank Matranga <thefrankmatranga@gmail.com>
2017-06-13 15:52:39 -0400

    Add Funds and See Recent

--- server/db/schemas/Funds.js
+++ server/db/schemas/Funds.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const fundsSchema = new Schema({
     location: { type: mongoose.Schema.Types.ObjectId, ref: 'Location' },
-    teacher: { type: Number, ref: 'User', required: true },
+    submittedBy: { type: Number, ref: 'User', required: true },
     amount: { type: Number, min: 0, required: true },
     method: { type: String, required: true },
     form: { type: String, enum: ['Cash', 'Check', 'Other'], default: 'Other' },
--- server/routes/fundraising/index.js
+++ server/routes/fundraising/index.js
@@ -8,12 +8,36 @@ router.get('/', (req, res, next) => {
     // Get recent funds for this location
     req.db.Funds.find({ location: req.user.location._id })
         .limit(10)
+        .populate('submittedBy')
         .exec()
         .then(fundsList => {
             res.locals.recentFunds = fundsList;
             res.render('fundraising/index');
         })
         .catch(next);
+});
+
+router.post('/addfunds', (req, res, next) => {
+    const locationId = req.user.location._id;
+    const submittedById = req.user._id;
+    const amount = req.body.amount;
+    const method = req.body.method;
+    const form = req.body.form;
+    const source = req.body.source;
+
+    const newFunds = new req.db.Funds({
+        location: locationId,
+        submittedBy: submittedById,
+        amount,
+        method,
+        form,
+        source
+    });
+
+    newFunds.save().then(() => {
+        req.flash('success', 'Added new funds for ' + req.user.location.name);
+        res.redirect('/fundraising');
+    }).catch(next);
 });
 
 module.exports = router;
