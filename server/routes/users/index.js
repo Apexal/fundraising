dd19d11bf595160276131676c4ea3383554d810e
Frank Matranga <thefrankmatranga@gmail.com>
2017-06-19 19:54:56 -0400

    User list start

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(requireVerified);
+
 /* GET home page. */
-router.get('/', (req, res) => {
-    res.render('users/index');
+router.get('/', (req, res, next) => {
+    req.db.User.find({ verified: true })
+        .exec()
+        .then(users => {
+            res.locals.users = users;
+            
+            res.locals.teachers = users.filter(u => u.rank == 0);
+            res.locals.directors = users.filter(u => u.rank == 1);
+            res.locals.ambassadors = users.filter(u => u.rank == 2);
+            res.locals.administrators = users.filter(u => u.rank == 3);
+
+            res.render('users/index');
+        })
+        .catch(next);
 });
 
 router.get('/:email', (req, res, next) => {
