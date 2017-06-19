a336a9d0c281c47f3c78f4ca7716b4d8b3261310
Frank Matranga <thefrankmatranga@gmail.com>
2017-06-19 16:27:41 -0400

    Added camp methods to get superiors

--- server/db/schemas/Camp.js
+++ server/db/schemas/Camp.js
@@ -2,15 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const campSchema = new Schema({
-    location: { type: mongoose.Schema.Types.ObjectId, ref: 'Location' },
+    location: { type: mongoose.Schema.Types.ObjectId, ref: 'Location', required: true },
+    info: String,
     startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true },
-    ambassador: { type: Number, ref: 'User' },
-    director: { type: Number, ref: 'User'}
+    endDate: { type: Date, required: true }
 });
 
-campSchema.methods.getTeachers = function(cb) {
-    return this.model('User').find({ location: this._id }).exec();
+campSchema.methods.getTeachers = function() {
+    console.log('Getting teachers for ...' + this._id);
+    return this.model('User').find({ rank: 0, currentCamp: this._id }).exec();
+}
+
+campSchema.methods.getDirector = function() {
+    console.log('Getting director...');
+    return this.model('User').findOne({ rank: 1, currentCamp: this._id }).exec();
+}
+
+campSchema.methods.getAmbassador = function() {
+    console.log('Getting ambassador...');
+    return this.model('User').findOne({ rank: 2, currentCamps: this._id }).exec();
 }
 
 module.exports = { name: 'Camp', schema: campSchema };
