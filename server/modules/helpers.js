1f3c7b017f053d16828006098cbbf0902e4059f3
Frank Matranga <thefrankmatranga@gmail.com>
2017-06-19 22:51:42 -0400

    Limit string length helper

--- server/modules/helpers.js
+++ server/modules/helpers.js
@@ -2,10 +2,12 @@
 
 module.exports = {
     activeLink: (href, current) => {
-
         current = (current == '/home' ? '/' : current);
         if ((href === '/' && current === href) || (href !== '/' && current.startsWith(href))) {
             return 'active';
         }
+    },
+    limit: (string, maxLength) => {
+        return (string.length > maxLength ? string.substring(0, maxLength) + '...' : string);
     }
 };
