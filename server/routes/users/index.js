e5eb7fa84ad96c177fc733c19f870058057796dc
Frank Matranga <thefrankmatranga@gmail.com>
2017-06-19 23:57:03 -0400

    Nasty User Profile/Edit Start

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -21,8 +21,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:email', (req, res, next) => {
-    const email = req.params.email;
-    req.db.User.findOne({ email: email })
+    req.db.User.findOne({ email: req.params.email })
         .populate('currentCamp')
         .populate('currentCamps')
         .populate('pastCamps')
@@ -32,6 +31,29 @@ router.get('/:email', (req, res, next) => {
 
             res.locals.targetUser = user;
             res.render('users/user');
+        })
+        .catch(next);
+});
+
+router.get('/:email/edit', requireAdmin, (req, res, next) => {
+    req.db.User.findOne({ email: req.params.email })
+        .populate('currentCamp')
+        .populate('currentCamps')
+        .exec()
+        .then(user => {
+            if (!user) throw new Error('Failed to find user.');
+
+            res.locals.targetUser = user;
+            return req.db.Camp.find()
+                .populate('location')
+                .exec();
+        })
+        .then(camps => {
+            res.locals.camps = camps;
+            res.locals.noDirector = camps.filter(c => !c.director);
+            res.locals.noAmbassador = camps.filter(c => !c.ambassador);
+
+            res.render('users/edit');
         })
         .catch(next);
 });
