0ec0503063bf020ef240a32ae7980f74e351a9c1
Frank Matranga <thefrankmatranga@gmail.com>
2017-06-19 21:54:14 -0400

    Determine camp active status

--- server/db/schemas/Camp.js
+++ server/db/schemas/Camp.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const moment = require('moment');
 
 const campSchema = new Schema({
     location: { type: mongoose.Schema.Types.ObjectId, ref: 'Location', required: true },
     info: String,
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true }
+}, {
+    toObject: {
+        virtuals: true
+    },
+    toJSON: {
+        virtuals: true 
+    }
 });
 
 campSchema.methods.getTeachers = function() {
@@ -22,5 +30,10 @@ campSchema.methods.getAmbassador = function() {
     console.log('Getting ambassador...');
     return this.model('User').findOne({ rank: 2, currentCamps: this._id }).exec();
 }
+
+campSchema.virtual('active').get(function() { 
+    // Determine whether camp is going on right now
+    return moment().isBetween(moment(self.startDate), moment(self.endDate));
+});
 
 module.exports = { name: 'Camp', schema: campSchema };
