ae5965c59e13c86fd7735076f87643401f2cde41
Frank Matranga <thefrankmatranga@gmail.com>
2017-07-16 17:17:25 -0400

    More help info

--- server/modules/helpInfo.js
+++ server/modules/helpInfo.js
@@ -1,8 +1,17 @@
 const helpInfo = {};
 
+helpInfo['latest-news'] = "Latest news info here...";
+
 helpInfo['camps'] = "Camp info here...";
+helpInfo['active-camps'] = "Active camp info here...";
+helpInfo['inactive-camps'] = "Inactive camp info here...";
+
 helpInfo['locations'] = "Location info here...";
-helpInfo['active-involvements'] = "Help here!";
+helpInfo['active-locations'] = "Active location info here...";
+helpInfo['inactive-locations'] = "Inactive location info here...";
+
+helpInfo['active-involvements'] = "Active involvement info here...";
+
 helpInfo['unverified-users'] = "New users that login are restricted from doing anything until an Administrator verifies them.";
 
 module.exports = helpInfo;
