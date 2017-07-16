b8f81fce63d758f360334d048b450d87f504b587
Frank Matranga <thefrankmatranga@gmail.com>
2017-07-16 15:32:44 -0400

    More help info

--- server/modules/helpInfo.js
+++ server/modules/helpInfo.js
@@ -1,5 +1,8 @@
 const helpInfo = {};
 
+helpInfo['camps'] = "Camp info here...";
+helpInfo['locations'] = "Location info here...";
 helpInfo['active-involvements'] = "Help here!";
+helpInfo['unverified-users'] = "New users that login are restricted from doing anything until an Administrator verifies them.";
 
 module.exports = helpInfo;
