1eddd059a97fcba745d1077ce299ee7abc474312
Frank Matranga <thefrankmatranga@gmail.com>
2017-07-21 17:48:22 -0400

    Fake users for testing

--- server/db/index.js
+++ server/db/index.js
@@ -20,4 +20,71 @@ fs.readdirSync(__dirname + '/schemas')
     });
 console.log('Loaded schemas...');
 
+
+const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+
+const fakeTeacher = {
+    _id: 1,
+    email: 'teacher@kidstales.org',
+    age: 17,
+    grade: 12,
+    phoneNumber: '888-888-8888',
+    location: 'NYC',
+    name: { first: 'Teacher', last: 'Test' },
+    rank: 'teacher',
+    rankInfo: {},
+    application: { why: 'Why not?' },
+    registeredDate: Date.now(),
+    admin: false,
+    verified: true
+};
+models.User.findOneAndUpdate({ _id: 1 }, fakeTeacher, options, function(error, result) {
+    if (error) return;
+    console.log(result);
+    // do something with the document
+});
+
+
+const fakeDirector = {
+    _id: 2,
+    email: 'director@kidstales.org',
+    age: 17,
+    grade: 12,
+    phoneNumber: '888-888-8888',
+    location: 'NYC',
+    name: { first: 'Director', last: 'Test' },
+    rank: 'director',
+    rankInfo: {},
+    application: { why: 'Because.' },
+    registeredDate: Date.now(),
+    admin: false,
+    verified: true
+};
+models.User.findOneAndUpdate({ _id: 2 }, fakeDirector, options, function(error, result) {
+    if (error) return;
+    console.log(result);
+    // do something with the document
+});
+
+const fakeAmbassador = {
+    _id: 3,
+    email: 'ambassador@kidstales.org',
+    age: 17,
+    grade: 12,
+    phoneNumber: '888-888-8888',
+    location: 'NYC',
+    name: { first: 'Ambassador', last: 'Test' },
+    rank: 'ambassador',
+    rankInfo: {},
+    application: { why: 'idek.' },
+    registeredDate: Date.now(),
+    admin: false,
+    verified: true
+};
+models.User.findOneAndUpdate({ _id: 3 }, fakeAmbassador, options, function(error, result) {
+    if (error) return;
+    console.log(result);
+    // do something with the document
+});
+
 module.exports = models;
