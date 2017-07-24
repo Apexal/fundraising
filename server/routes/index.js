f89d2e9234db309672b71d3543dc6da79464c77f
Frank Matranga <thefrankmatranga@gmail.com>
2017-07-23 20:14:55 -0400

    Edit profile improved and simplified

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
+const easyimg = require('easyimage');
+const fs = require('fs');
+
+const IMAGE_TYPES = ['image/jpeg', 'image/png'];
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, __dirname + '/../../client/public/images'),
+    filename: (req, file, cb) => cb(null, 'user-' + req.user._id + '.' + file.originalname.split('.')[1]),
+    fileFilter: (req, file, cb) => {
+        if(IMAGE_TYPES.includes(file.mimetype)) {
+            cb(null, true)
+        } else {
+            cb(null, false)
+        }
+    }
+});
+
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 1000000, // 1 Megabyte
+        files: 1,
+    }
+});
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -25,9 +51,9 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/loginas', /*requireAdmin,*/ (req, res, next) => {
-    if (!req.query.email) return next(new Error('Invalid user email.'));
+    //if (!req.query.email) return next(new Error('Invalid user email.'));
 
-    req.db.User.findOne({ email: req.query.email })
+    req.db.User.findById(req.query.id)//One({ email: req.query.email })
         .exec()
         .then(user => {
             return req.logIn(user, err => {
@@ -44,6 +70,65 @@ router.get('/logout', function(req, res){
     req.logout();
     req.flash('info', 'Successful logout.');
     res.redirect('/');
+});
+
+router.get('/profile', requireVerified, (req, res, next) => {
+    res.locals.pageTitle = 'Your Profile';
+
+    res.render('index/profile');
+});
+
+router.post('/profile', requireVerified, upload.single('profileImage'), (req, res, next) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const grade = req.body.grade;
+    const age = req.body.age;
+    const phoneNumber = req.body.phoneNumber;
+    const location = req.body.location;
+
+    req.user.name.first = firstName;
+    req.user.name.last = lastName;
+    req.user.grade = grade;
+    req.user.age = age;
+    req.user.phoneNumber = phoneNumber;
+    req.user.location = location;
+
+    if (req.file) {
+        req.user.profileImageName = req.file.filename;
+        const imgPath = path.join(__dirname, '..', '..', 'client', 'public', 'images', req.file.filename);
+        console.log(imgPath);
+
+        return easyimg.resize({
+            src: imgPath, dst: imgPath,
+            width: 200, height: 250,
+        })
+        .then(image => {
+            const newPath = imgPath.replace('.' + imgPath.split('.')[1], '.jpg');
+
+            return easyimg.convert({
+                src: imgPath, dst: newPath, quality: 90
+            });
+        }).then(image => {
+            if (req.user.profileImageName !== image.name) {
+                fs.unlinkSync(imgPath, err => {
+                    if (err) console.error(err);
+                });
+            }
+            req.user.profileImageName = image.name;
+            return req.user.save();
+        }).then(user => {
+            req.flash('success', 'Successfully updated profile and photo.');
+            res.redirect('/profile');
+        })
+        .catch(next);
+    }
+
+    return req.user.save()
+        .then(user => {
+            req.flash('success', 'Successfully updated profile.');
+            res.redirect('/profile');
+        })
+        .catch(next);
 });
 
 module.exports = router;
