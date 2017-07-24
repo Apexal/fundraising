9048d032b1ccbeeeef1fb095625e2c43a2fd6076
Frank Matranga <thefrankmatranga@gmail.com>
2017-07-23 22:32:21 -0400

    Removed unverified page

--- server/routes/administration/index.js
+++ server/routes/administration/index.js
@@ -25,31 +25,4 @@ router.get('/fundraising', (req, res, next) => {
         .catch(next);
 });
 
-router.get('/unverified', (req, res, next) => {
-    req.db.User.find({ verified: false })
-        .exec()
-        .then(users => {
-            res.locals.unverifiedUsers = users;
-
-            res.render('administration/unverified');
-        })
-        .catch(next);
-});
-
-router.post('/verify/:id', (req, res, next) => {
-    req.db.User.findById(req.params.id)
-        .exec()
-        .then(user => {
-            user.verified = true;
-            return user.save();
-        })
-        .then((user) => {
-            const message = "<h3>Your account has just been verified by an Administrator!</h3><p>You can now use the services on the KidsTales web app.</p>";
-            sendEmail(user.email, "You've Been Verified", message);
-            req.flash('success', `Successfully verified and emailed ${user.name.full}.`);
-            res.redirect('/administration/unverified');
-        })
-        .catch(next);
-});
-
 module.exports = router;
