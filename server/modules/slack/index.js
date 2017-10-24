86f70b0a08f3599faca3aa0dacd3d9d8f46f98ca
Frank Matranga <Apexal@users.noreply.github.com>
2017-10-24 16:51:19 -0400

    Untested automate Slack invite

--- server/modules/slack/index.js
+++ server/modules/slack/index.js
@@ -1,3 +1,19 @@
 const config = require('config');
 
-module.exports = {};
+const token = config.get('slack.webToken');
+const teamInviteURL = `https://${config.get('slack.teamName')}.slack.com/api/users.admin.invite?t=`;
+const channels = config.get('slack.startChannels');
+
+const inviteToTeam = (firstName, email) => {
+    const url = teamInviteUrl + (new Date/1E3|0);
+    const data = {
+        email,
+        channels: channels.join(','),
+        first_name: firstName,
+        token,
+        set_active: true,
+        _attempts: 1
+    };
+};
+
+module.exports = { inviteToTeam };
