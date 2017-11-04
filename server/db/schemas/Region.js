ae13e91bccfca003b4e81910b9c8e952979fdefb
Frank Matranga <thefrankmatranga@gmail.com>
2017-11-04 17:25:45 -0400

    Update region schema

--- server/db/schemas/Region.js
+++ server/db/schemas/Region.js
@@ -4,7 +4,7 @@ const mongoosePaginate = require('mongoose-paginate');
 
 const regionSchema = new Schema({
     name: { type: String, trim: true, unique: true, required: true, maxlength: 30 },
-    center: { type: String, trim: true, unique: true, maxlength: 200 },
+    center: { type: String, trim: true, maxlength: 200 },
     description: { type: String, trim: true, maxlength: 2000 },
     dateAdded: { type: Date, required: true }
 });
