cb7d0c67f7316beab5f9d9a38284277dcf7cc80c
Frank Matranga <thefrankmatranga@gmail.com>
2018-02-08 23:00:05 -0500

    Help info

--- server/modules/helpInfo.js
+++ server/modules/helpInfo.js
@@ -15,7 +15,7 @@ helpInfo['teachers'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 
 helpInfo['latest-news'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin libero mollis libero dictum, non mattis urna eleifend.";
 
-helpInfo['workshops'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin libero mollis libero dictum, non mattis urna eleifend.";
+helpInfo['workshops'] = "During a Kids Tales workshop, 8- to 12-year-old kids spend one week brainstorming, writing, and editing their very own short story. At the end of the week, the stories are assembled in a collection and published as an anthology on Amazon.com. Each student writer receives a copy of the book, as well as his or her very own journal and pencils.";
 helpInfo['active-workshops'] = "Active workshops are starting soon, in progress, or have very recently ended.";
 helpInfo['inactive-workshops'] = "Past workshops have finished and have been archived. Their info cannot be changed.";
 
