79ef458ee8cf35ca892c1b2a02cdcf7e72f96fb7
Frank Matranga <thefrankmatranga@gmail.com>
2018-03-08 13:29:07 -0500

    Show fund total on workshop modal (closes #47)

---
+++ client/public/javascripts/workshopModals.js
@@ -0,0 +1,6 @@
+$('.workshop-modal').on('shown.bs.modal', function () {
+    const workshopId = $(this).data('workshop-id');
+    $.getJSON('/api/workshops/' + workshopId, (data, err) => {
+        $(this).find('.funds-total').text(`$${data.fundraising.total} total`);
+    });
+});
