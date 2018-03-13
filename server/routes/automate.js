f00944883249fc345414b839865e76256e10bcc4
Frank Matranga <thefrankmatranga@gmail.com>
2018-03-13 00:21:05 -0400

    [Untested] Transfer

--- server/routes/automate.js
+++ server/routes/automate.js
@@ -4,17 +4,39 @@ const config = require('config');
 
 /* Validate request */
 router.use((req, res, next) => {
-  if (req.body.secret == config.get('secret')) return next();
+    if (req.body.secret == config.get('secret')) return next();
   
-  res.status(500);
-  res.json({ error: 'Invalid secret.' });
+    res.status(500);
+    res.json({ error: 'Invalid secret.' });
 });
 
 /* Transfer from survey monkey to website */
 router.post('/transfer', (req, res, next) => {
-  // Create user
-  // Invite to slack
-  // Send emails
+    // Create user
+    const sm = req.body;
+    const region = 0; //???
+    const user = {
+        region,
+        name: {
+            full: sm['firstName'] + ' ' + sm['lastName'],
+            first: sm['firstName'],
+            last: sm['lastName']
+        },
+        application: {
+            applying: true,
+            rank: sm['rank'],
+            recommender: sm['recommender'],
+            why: sm['why'],
+            updatedAt: sm['when']
+        },
+        verified: false
+    }
+
+    ['email', 'age', 'grade', 'phoneNumber'].forEach(v => user[v] = sm[v]);
+    // Invite to slack
+    // Send emails
+
+    res.json(user);
 });
 
 module.exports = router;
