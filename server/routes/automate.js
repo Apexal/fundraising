e46511005d5647819f352a315201b340516966b1
Frank Matranga <thefrankmatranga@gmail.com>
2018-03-13 23:20:50 -0400

    Working transfer

--- server/routes/automate.js
+++ server/routes/automate.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const config = require('config');
+const ObjectId = require('mongodb').ObjectID;
+const slack = require('../modules/slack');
 
 /* Validate request */
 router.use((req, res, next) => {
@@ -14,7 +16,7 @@ router.use((req, res, next) => {
 router.post('/transfer', (req, res, next) => {
     // Create user
     const sm = req.body;
-    const region = 0; //???
+    const region = 0; //REPLACE WITH REGION OBJECT ID
     const user = {
         region,
         name: {
@@ -23,20 +25,48 @@ router.post('/transfer', (req, res, next) => {
             last: sm['lastName']
         },
         application: {
-            applying: true,
+            applying: false,
             rank: sm['rank'],
             recommender: sm['recommender'],
             why: sm['why'],
             updatedAt: sm['when']
         },
-        verified: false
+        verified: true
     }
 
-    ['email', 'age', 'grade', 'phoneNumber'].forEach(v => user[v] = sm[v]);
-    // Invite to slack
+    const matching = ['email', 'age', 'grade', 'phoneNumber', 'rank'];
+    for(let i = 0; i < matching.length; i++) {
+       user[matching[i]] = sm[matching[i]]; 
+    }
+
+    req.db.User.findOneAndUpdate({ email: sm['email'], verified: false }, user, { upsert: true, new: true, setDefaultsOnInsert: true })
+        .exec()
+        .then(user => {
+            if (!user) throw new Error('No such user.');
+
+            log(user, 'transfer', `Transfer ${user.name.full} as ${user.rank}.`);
+            return user.save();
+        })
+        .then(user => {
+            req.user = user;
+            // Invite to slack
+            return slack.inviteToTeam(req.user.name.first, req.user.email);
+        })
+        .then(data => {
+            data = JSON.parse(data);
+
+            if (data.ok) {
+                return res.json({ success: true, message: "Successful transfer and slack invite" });
+            } else if (data.error == "already_in_team") {
+                return res.json({ success: true, message: "Transfered, they are already on Slack." });
+            } else {
+                throw new Error("Failed to invite to Slack.");
+            }
+        })
+        .catch(next);
+    
     // Send emails
 
-    res.json(user);
 });
 
 module.exports = router;
